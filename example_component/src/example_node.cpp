d8c5841cd33390764e8a02c93e401917d2ebfd66
Changchun Lee <lichangchun816@163.com>
2018-12-18 23:31:46 +0800

    added a rosspa launch file

--- example_component/src/example_node.cpp
+++ example_component/src/example_node.cpp
@@ -37,7 +37,7 @@ void MyApplication::run()
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "example_component");
+  ros::init(argc, argv, "example_node");
 
   spa::uuid_t cuuid = {0x01234567, 0x89ab, 0xcdef, 0x01, 0x23, {0x45, 0x67, 0x89, 0xab, 0xcd, 0xef}};
   spa::MyApplication myApp(cuuid, spa::SPA_CMPTYPE_UNKNOWN, ros::package::getPath("example_component") + "/xteds/Thermometer_Demo.xml");
